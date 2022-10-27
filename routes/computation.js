bade5ba7ec224297b679d951568fefbce992be69
Swathi_Nuli_S555089 <88883122+nswathi27@users.noreply.github.com>
2022-10-26 22:45:47 -0500

    added new route computation

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+ var min = 20;
+var max = 24;
+// var random_value = Math.random() * (max - min) + min; //Generates a random value between min and max-1
+
+
+// var rand1 = Math.random() * (max - min) + min;
+// var rand2 = Math.random() * (max - min) + min;
+// var rand3 = Math.random() * (max - min) + min;
+// var rand4 = Math.random() * (max - min) + min;
+
+// var abs = Math.abs(rand1);
+// var cos = Math.acos(rand2) 
+// var sin = Math.sin(rand3);
+// var sinh = Math.sinh(rand4);
+
 /* GET mydata page. */
+
+var rand1 = Math.random() * (max - min) + min;
+
 router.get('/', function(req, res, next) {
-  res.render(`Math.cos() applied to {x} is {y}`);
+
+ if(req.query.x!=undefined) x =req.query.x;
+else x=rand1;
+
+var math_result = Math.sinh(x);
+
+  
+  res.render('computation', {x:x, math_result:math_result});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var times = 0;
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/', function(req, res, next) {
   times = times + 1;
   res.send(`User accesses are: ${times}`);
 });
